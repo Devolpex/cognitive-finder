a4c9c75e5eb1a36aebf8fe5ce97bb531cb3c4513
Marouane Dbibih <marrouane.dbibih@gmail.com>
2024-12-28 22:10:26 +0100

    update the tracking microservice

--- tracking-ms/src/main/java/org/cognitivefinder/tracking/modules/device/DeviceRepository.java
+++ tracking-ms/src/main/java/org/cognitivefinder/tracking/modules/device/DeviceRepository.java
@@ -4,11 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DeviceRepository extends JpaRepository<Device, Long> {
-
+    
+    @Query("SELECT d FROM Device d WHERE d.patientId = :patientId")
     Optional<Device> findByPatientId(String patientId);
 
     List<Device> findByPatientIdIn(List<String> patientIds);
