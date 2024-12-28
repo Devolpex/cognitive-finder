6f76571136ac1c2ebaacbd86cd19e4fb41a56073
Marouane Dbibih <marrouane.dbibih@gmail.com>
2024-12-28 17:31:10 +0100

    Develop the sidbar and navbar and adjust the admin layout

--- web-app/src/components/Sidebar.tsx
+++ web-app/src/components/Sidebar.tsx
@@ -8,18 +8,36 @@ import {
   Chip,
 } from "@material-tailwind/react";
 import {
-  PresentationChartBarIcon,
-  ShoppingBagIcon,
   UserCircleIcon,
-  Cog6ToothIcon,
-  InboxIcon,
   PowerIcon,
+  UserGroupIcon,
 } from "@heroicons/react/24/solid";
+
+// Define a menuItems array with name, path, and icon
+const menuItems = [
+  {
+    title: "Patients",
+    icon: UserGroupIcon,
+    path: "/admin/patients",
+  },
+
+  {
+    title: "Profile",
+    icon: UserCircleIcon,
+    path: "/profile",
+  },
+
+  {
+    title: "Log Out",
+    icon: PowerIcon,
+    path: "/logout",
+  },
+];
 
 export function Sidebar() {
   return (
     <Card
-      className="h-[calc(100vh-2rem)] w-full max-w-[20rem] p-4 shadow-xl shadow-blue-gray-900/5"
+      className="h-[calc(100vh-2rem)] w-[20rem] max-w-[20rem] p-4 shadow-xl shadow-blue-gray-900/5"
       placeholder={undefined}
       onPointerEnterCapture={undefined}
       onPointerLeaveCapture={undefined}
@@ -40,103 +58,39 @@ export function Sidebar() {
         onPointerEnterCapture={undefined}
         onPointerLeaveCapture={undefined}
       >
-        <ListItem
-          placeholder={undefined}
-          onPointerEnterCapture={undefined}
-          onPointerLeaveCapture={undefined}
-        >
-          <ListItemPrefix
+        {/* Map through menuItems to dynamically generate the sidebar menu */}
+        {menuItems.map((item) => (
+          <ListItem
+            key={item.title}
             placeholder={undefined}
             onPointerEnterCapture={undefined}
             onPointerLeaveCapture={undefined}
           >
-            <PresentationChartBarIcon className="h-5 w-5" />
-          </ListItemPrefix>
-          Dashboard
-        </ListItem>
-        <ListItem
-          placeholder={undefined}
-          onPointerEnterCapture={undefined}
-          onPointerLeaveCapture={undefined}
-        >
-          <ListItemPrefix
-            placeholder={undefined}
-            onPointerEnterCapture={undefined}
-            onPointerLeaveCapture={undefined}
-          >
-            <ShoppingBagIcon className="h-5 w-5" />
-          </ListItemPrefix>
-          E-Commerce
-        </ListItem>
-        <ListItem
-          placeholder={undefined}
-          onPointerEnterCapture={undefined}
-          onPointerLeaveCapture={undefined}
-        >
-          <ListItemPrefix
-            placeholder={undefined}
-            onPointerEnterCapture={undefined}
-            onPointerLeaveCapture={undefined}
-          >
-            <InboxIcon className="h-5 w-5" />
-          </ListItemPrefix>
-          Inbox
-          <ListItemSuffix
-            placeholder={undefined}
-            onPointerEnterCapture={undefined}
-            onPointerLeaveCapture={undefined}
-          >
-            <Chip
-              value="14"
-              size="sm"
-              variant="ghost"
-              color="blue-gray"
-              className="rounded-full"
-            />
-          </ListItemSuffix>
-        </ListItem>
-        <ListItem
-          placeholder={undefined}
-          onPointerEnterCapture={undefined}
-          onPointerLeaveCapture={undefined}
-        >
-          <ListItemPrefix
-            placeholder={undefined}
-            onPointerEnterCapture={undefined}
-            onPointerLeaveCapture={undefined}
-          >
-            <UserCircleIcon className="h-5 w-5" />
-          </ListItemPrefix>
-          Profile
-        </ListItem>
-        <ListItem
-          placeholder={undefined}
-          onPointerEnterCapture={undefined}
-          onPointerLeaveCapture={undefined}
-        >
-          <ListItemPrefix
-            placeholder={undefined}
-            onPointerEnterCapture={undefined}
-            onPointerLeaveCapture={undefined}
-          >
-            <Cog6ToothIcon className="h-5 w-5" />
-          </ListItemPrefix>
-          Settings
-        </ListItem>
-        <ListItem
-          placeholder={undefined}
-          onPointerEnterCapture={undefined}
-          onPointerLeaveCapture={undefined}
-        >
-          <ListItemPrefix
-            placeholder={undefined}
-            onPointerEnterCapture={undefined}
-            onPointerLeaveCapture={undefined}
-          >
-            <PowerIcon className="h-5 w-5" />
-          </ListItemPrefix>
-          Log Out
-        </ListItem>
+            <ListItemPrefix
+              placeholder={undefined}
+              onPointerEnterCapture={undefined}
+              onPointerLeaveCapture={undefined}
+            >
+              <item.icon className="h-5 w-5" />
+            </ListItemPrefix>
+            {item.title}
+            {item.badge && (
+              <ListItemSuffix
+                placeholder={undefined}
+                onPointerEnterCapture={undefined}
+                onPointerLeaveCapture={undefined}
+              >
+                <Chip
+                  value={item.badge}
+                  size="sm"
+                  variant="ghost"
+                  color="blue-gray"
+                  className="rounded-full"
+                />
+              </ListItemSuffix>
+            )}
+          </ListItem>
+        ))}
       </List>
     </Card>
   );
--- web-app/src/layout/AdminLayout.tsx
+++ web-app/src/layout/AdminLayout.tsx
@@ -1,13 +1,21 @@
-import * as React from 'react';
+import React from 'react';
 import { Outlet } from 'react-router-dom';
+import { DefaultNavbar } from '../components/Navbar';
+import { Sidebar } from '../components/Sidebar';
 
-export interface IAdminLayoutProps {
-}
+export interface IAdminLayoutProps {}
 
-export function AdminLayout (props: IAdminLayoutProps) {
+export function AdminLayout(props: IAdminLayoutProps) {
   return (
-    <div>
-      <Outlet />
+    <div className="flex min-h-screen py-4 px-4 bg-blue-gray-50 w-full h-auto gap-4">
+      {/* Sidebar */}
+      <Sidebar />
+      
+      {/* Main Content */}
+      <main className="flex flex-col ml-[20rem] px-16 gap-4 w-full">
+        <DefaultNavbar />
+        <Outlet />
+      </main>
     </div>
   );
 }
